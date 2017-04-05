356f2153e6de11f712788654c455a2002f405cae
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-04-05 15:29:56 +0300

    Initial commit

---
+++ src/UkLocale.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-time
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-time
+ */
+declare(strict_types = 1);
+
+namespace Vainyl\Locale;
+
+use Vainyl\Core\Id\AbstractIdentifiable;
+
+/**
+ * Class UkLocale
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class UkLocale extends AbstractIdentifiable implements LocaleInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getName() : string
+    {
+        return 'uk';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getWeekStartsAt(): int
+    {
+        return 1;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getWeekEndsAt(): int
+    {
+        return 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getWeekendDays(): array
+    {
+        return [0, 6];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDefaultFormat(): string
+    {
+        return 'Y-m-d H:i:s';
+    }
+}
---
+++ src/UsLocale.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-time
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-time
+ */
+declare(strict_types = 1);
+
+namespace Vainyl\Locale;
+
+use Vainyl\Core\Id\AbstractIdentifiable;
+
+/**
+ * Class UsLocale
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class UsLocale extends AbstractIdentifiable implements LocaleInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getName() : string
+    {
+        return 'us';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getWeekStartsAt(): int
+    {
+        return 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getWeekEndsAt(): int
+    {
+        return 6;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getWeekendDays(): array
+    {
+        return [0, 6];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDefaultFormat(): string
+    {
+        return 'Y-m-d H:i:s';
+    }
+}
