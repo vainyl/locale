f3294d0c7d8336337c2e18a97d8f40e230c572db
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-05-10 15:29:47 +0300

    Added env

--- src/Extension/LocaleExtension.php
+++ src/Extension/LocaleExtension.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Vainyl\Locale\Extension;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Vainyl\Core\Application\EnvironmentInterface;
 use Vainyl\Core\Extension\AbstractExtension;
 
 /**
@@ -25,11 +26,11 @@ class LocaleExtension extends AbstractExtension
     /**
      * @inheritDoc
      */
-    public function load(array $configs, ContainerBuilder $container): AbstractExtension
+    public function load(array $configs, ContainerBuilder $container, EnvironmentInterface $environment = null): AbstractExtension
     {
         $container
             ->addCompilerPass(new LocaleCompilerPass());
 
-        return parent::load($configs, $container);
+        return parent::load($configs, $container, $environment);
     }
 }
